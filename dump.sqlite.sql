cdd8d967c6d3f0dd4103f8c1befb76ca95fbf7e7
pa-augustord <159174243+pa-augustord@users.noreply.github.com>
2024-08-13 13:49:10 -0300

    Add files via upload
    Aula 13 08

---
+++ dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE Alunos(
+  id int(3) UNIQUE,
+  nome varchar (30),
+  idade varchar (3),
+  turma varchar (4)
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
